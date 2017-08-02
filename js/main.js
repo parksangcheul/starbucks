7ea46c54070e3968b5056b79512fd765721ca9fa
parksc <zxc739@gmail.com>
2017-08-03 01:11:44 +0900

    프로젝트 생성!

--- js/main.js
+++ js/main.js
@@ -22,24 +22,23 @@
     }
 
     function toggleTopCard() {
-      $('.toggle-top-card').on(
-          {click: function () {
-          _sb.$topCard.slideToggle(400);
-      },
-
-          mouseenter: function () {
+      $('.toggle-top-card').on({
+              click: function () {
+                  _sb.$topCard.slideToggle(400);
+              },
+                mouseenter: function () {
               animateToggleTopCardBtn();
           }
-
       })
-    };
+    }
 
     function animateToggleTopCardBtn() {
         TweenMax.to('.toggle-top-card .cup', 1.5, {
             y: -30,
             repeat: -1,
             yoyo: true
-        });
+        })
+    };
 
 
 
